602f617bbc5fefd0de42299be782f9381635f1ba
joshwlambert <j.w.l.lambert@rug.nl>
2020-12-30 20:11:54 +0000

    updated check_island_state

--- R/DAISIE_utils.R
+++ R/DAISIE_utils.R
@@ -665,14 +665,26 @@ sim_mainland <- function(
 #'
 #' @inheritParams default_params_doc
 #'
-#' @return matrix
+#' @return a list
 check_island_state <- function(timeval,
                                island_spec,
-                               mainland) {
-  if (timeval > mainland[, 9] && any(island_spec[, 4] == "I")) {
-    immig_spec <- which(island_spec[, 4] == "I")
-    island_spec[immig_spec, 4] <- "A"
-    island_spec[immig_spec, 7] <- "mainland_extinction"
+                               mainland,
+                               stt_table) {
+  if (any(island_spec[, 4] == "I")) {
+    immig_spec <- island_spec[which(island_spec[, 4] == "I"), 1]
+    for (i in immig_spec) {
+      immig_spec_ex_time <- mainland[which(mainland[, 1] == immig_spec), 9]
+      if (timeval > immig_spec_ex_time) {
+        island_spec[which(island_spec[, 1] == immig_spec), 4] <- "A"
+        island_spec[which(island_spec[, 1] == immig_spec), 7] <-
+          "mainland_extinction"
+      }
+    }
+    stt_table[nrow(stt_table), 2:4] <- c(
+      length(which(island_spec[, 4] == "I")),
+      length(which(island_spec[, 4] == "A")),
+      length(which(island_spec[, 4] == "C")))
   }
-  return(island_spec)
+  return(list(island_spec = island_spec,
+              stt_table = stt_table))
 }
--- tests/testthat/test-check_island_state.R
+++ tests/testthat/test-check_island_state.R
@@ -1,27 +1,42 @@
 context("check_island_state")
 
 test_that("check_island_state produces correct output", {
-  island_spec <- check_island_state(
+  mainland <- rbind(
+    c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
+    c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
+      "0.541999222479509"),
+    c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
+      "1"))
+  stt_table <- matrix(ncol = 4)
+  stt_table[1, ] <- c(1, 0, 0, 0)
+
+  island_state <- check_island_state(
     timeval = 0.05858136,
     island_spec = NULL,
-    mainland = rbind(
-      c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
-      c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
-        "0.541999222479509"),
-      c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
-        "1")))
-  expect_null(island_spec)
+    mainland = mainland,
+    stt_table = stt_table)
+  expect_true(is.list(island_state))
+  expect_null(island_state$island_spec)
+  expect_equal(island_state$stt_table, stt_table)
 
-  island_spec <- check_island_state(
-    timeval = 0.4508085,
+  mainland <- rbind(
+    c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
+    c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
+      "0.541999222479509"),
+    c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
+      "1"))
+  stt_table <- matrix(ncol = 4)
+  stt_table[1, ] <- c(1, 0, 0, 0)
+  stt_table <- rbind(stt_table, c(0.7070932, 1, 0, 0))
+
+  island_state <- check_island_state(
+    timeval = 0.6,
     island_spec = rbind(c("15", "15", "0.292906805531114", "I", NA, NA, NA)),
-    mainland = rbind(
-      c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
-      c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
-        "0.541999222479509"),
-      c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
-        "1")))
-  expect_equal(island_spec,
+    mainland = mainland,
+    stt_table = stt_table)
+  expect_equal(island_state$island_spec,
                rbind(c("15", "15", "0.292906805531114", "A", NA, NA,
                        "mainland_extinction")))
+  stt_table[2, ] <- c(0.7070932, 0, 1, 0)
+  expect_equal(island_state$stt_table, stt_table)
 })
