5cc39396c62a9ae1d7ff17d727626116fd792712
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-09 20:33:00 +0000

    bug fix in check_island_state and check_island_state tests

--- tests/testthat/test-check_island_state.R
+++ tests/testthat/test-check_island_state.R
@@ -1,42 +1,34 @@
 context("check_island_state")
 
-test_that("check_island_state produces correct output", {
+test_that("check_island_state produces correct output with empty island", {
   mainland <- rbind(
     c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
     c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
       "0.541999222479509"),
     c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
       "1"))
-  stt_table <- matrix(ncol = 4)
-  stt_table[1, ] <- c(1, 0, 0, 0)
 
   island_state <- check_island_state(
     timeval = 0.05858136,
+    totaltime = 1,
     island_spec = NULL,
-    mainland = mainland,
-    stt_table = stt_table)
-  expect_true(is.list(island_state))
-  expect_null(island_state$island_spec)
-  expect_equal(island_state$stt_table, stt_table)
+    mainland = mainland)
+})
 
+test_that("check_island_state produces correct output with non-empty island", {
   mainland <- rbind(
     c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
     c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
       "0.541999222479509"),
     c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
       "1"))
-  stt_table <- matrix(ncol = 4)
-  stt_table[1, ] <- c(1, 0, 0, 0)
-  stt_table <- rbind(stt_table, c(0.7070932, 1, 0, 0))
 
   island_state <- check_island_state(
+    totaltime = 1,
     timeval = 0.6,
     island_spec = rbind(c("15", "15", "0.292906805531114", "I", NA, NA, NA)),
-    mainland = mainland,
-    stt_table = stt_table)
-  expect_equal(island_state$island_spec,
+    mainland = mainland)
+  expect_equal(island_state,
                rbind(c("15", "15", "0.292906805531114", "A", NA, NA,
                        "mainland_extinction")))
-  stt_table[2, ] <- c(0.7070932, 0, 1, 0)
-  expect_equal(island_state$stt_table, stt_table)
 })
